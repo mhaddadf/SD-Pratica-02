e46cae5a98ff86fe298325d3d0e36ba1e9bf836d
Flavio <flavio.morais.ufla@gmail.com>
2018-03-29 20:50:50 -0300

    Tratamento do problema decorrente da falta de exibição de registros de cidades sem endereço completo

--- app/endereco.php
+++ app/endereco.php
@@ -22,20 +22,20 @@ class endereco extends baseClass {
         "
         SELECT
         tb3.endereco_codigo as id,
-        concat('Cep: ', tb3.endereco_cep ,' | Cidade: ', tb1.cidade_descricao, ' | Bairro: ', tb2.bairro_descricao, ' | Logradouro: ', tb3.endereco_logradouro) as label,
+        concat('Cep: ', ifnull(tb3.endereco_cep,tb1.cidade_cep) ,' | Cidade: ', tb1.cidade_descricao, ' | Bairro: ', ifnull(tb2.bairro_descricao,'Centro'), ' | Logradouro: ', ifnull(tb3.endereco_logradouro,'Centro')) as label,
         tb1.cidade_descricao as cidade,
-        tb2.bairro_descricao as bairro,
-        tb3.endereco_logradouro as logradouro,
-        tb3.endereco_cep as value
+        ifnull(tb2.bairro_descricao,'Centro') as bairro,
+        ifnull(tb3.endereco_logradouro,'Centro') as logradouro,
+        ifnull(tb3.endereco_cep,tb1.cidade_cep) as value
         from
         cep.cidade tb1
-        join
+        left join
         cep.bairro tb2
         on tb1.cidade_codigo = tb2.cidade_codigo
-        JOIN
+        left JOIN
         cep.endereco tb3
         on tb2.bairro_codigo = tb3.bairro_codigo
-        where (tb3.endereco_cep like '%$term%')
+        where (ifnull(tb3.endereco_cep,tb1.cidade_cep) like '%$term%')
         order by
         tb1.cidade_descricao,
         tb2.bairro_descricao,
